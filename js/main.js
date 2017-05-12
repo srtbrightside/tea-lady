819f92fcbef8ae0796125166f4ef0b82c371102d
Natalia Villalobos <n.v@outlook.cl>
2017-05-12 10:22:07 -0300

    Iniciando Ejercicio

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var nombre = document.getElementById("nombre");
+var email = document.getElementById("email");
+var direccion = document.getElementById("dirección");
+var telefono = document.getElementById("telefono");
+
+var 
